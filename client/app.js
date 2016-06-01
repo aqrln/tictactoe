99312729415e289e291eeeeefef211a798f34493
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-06-01 09:50:40 +0300

    Implement websocket connection

--- client/app.js
+++ client/app.js
@@ -7,4 +7,20 @@
 
   var wsProtocol = protocols[document.location.protocol];
 
+  var path = document.location.pathname,
+      gameId = path.slice(path.lastIndexOf('/') + 1);
+
+  var server = document.location.hostname;
+  if (document.location.port !== '80') {
+    server += ':' + document.location.port;
+  }
+
+  var wsUrl = wsProtocol + server + '/' + gameId;
+
+  var ws = new WebSocket(wsUrl);
+
+  ws.onopen = function () {
+    console.log('connected to server');
+  }
+
 })();
